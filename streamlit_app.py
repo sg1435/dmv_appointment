0b86e59efed6edc25e25ea3a4528751abfd40d9f
serhat <sguven1@yandex.com>
2022-04-01 08:16:34 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,19 +1,20 @@
+!pip install selenium
+!apt-get update # to update ubuntu to correctly run apt install
+!apt install chromium-chromedriver
+!cp /usr/lib/chromium-browser/chromedriver /usr/bin
+import sys
+sys.path.insert(0,'/usr/lib/chromium-browser/chromedriver')
+from selenium import webdriver
+import pandas as pd
+from datetime import date
+from selenium.webdriver.common.by import By
+chrome_options = webdriver.ChromeOptions()
+chrome_options.add_argument('--headless')
+chrome_options.add_argument('--no-sandbox')
+chrome_options.add_argument('--disable-dev-shm-usage')
+wd = webdriver.Chrome('chromedriver',chrome_options=chrome_options)
+
 def app_check():
-  !pip install selenium
-  !apt-get update # to update ubuntu to correctly run apt install
-  !apt install chromium-chromedriver
-  !cp /usr/lib/chromium-browser/chromedriver /usr/bin
-  import sys
-  sys.path.insert(0,'/usr/lib/chromium-browser/chromedriver')
-  from selenium import webdriver
-  import pandas as pd
-  from datetime import date
-  from selenium.webdriver.common.by import By
-  chrome_options = webdriver.ChromeOptions()
-  chrome_options.add_argument('--headless')
-  chrome_options.add_argument('--no-sandbox')
-  chrome_options.add_argument('--disable-dev-shm-usage')
-  wd = webdriver.Chrome('chromedriver',chrome_options=chrome_options)
   wd.get('https://telegov.njportal.com/njmvc/AppointmentWizard/19')
   tarihler = []
   listeler = [i.text for i in wd.find_elements(By.XPATH,"//*[contains(@id, 'dateText')]")]
