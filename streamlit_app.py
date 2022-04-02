a16fd0741f299415676bcd437dd0d236faa2745f
serhat <sguven1@yandex.com>
2022-04-02 12:56:21 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -7,27 +7,21 @@ from selenium.webdriver.common.by import By
 import streamlit as st
 
 
-
-def app_check(argument):
-  chrome_options = webdriver.ChromeOptions()
-  chrome_options.add_argument('--headless')
-  chrome_options.add_argument('--no-sandbox')
-  chrome_options.add_argument('--disable-dev-shm-usage')
-  wd = webdriver.Chrome('chromedriver',chrome_options=chrome_options)
-  wd.get('https://telegov.njportal.com/njmvc/AppointmentWizard/19')
-  tarihler = []
-  listeler = [i.text for i in wd.find_elements(By.XPATH,"//*[contains(@id, 'dateText')]")]
-  for i in listeler:
-    if i == 'No Appointments Available':
-      pass
-    else:
-      tarihler.append((pd.to_datetime(i.split(' ')[4], format='%m/%d/%Y') - pd.to_datetime(date.today(), format='%Y/%m/%d')).days)
-  argument = str(min(tarihler))
+chrome_options = webdriver.ChromeOptions()
+chrome_options.add_argument('--headless')
+chrome_options.add_argument('--no-sandbox')
+chrome_options.add_argument('--disable-dev-shm-usage')
+wd = webdriver.Chrome('chromedriver',chrome_options=chrome_options)
+wd.get('https://telegov.njportal.com/njmvc/AppointmentWizard/19')
+tarihler = []
+listeler = [i.text for i in wd.find_elements(By.XPATH,"//*[contains(@id, 'dateText')]")]
+for i in listeler:
+  if i == 'No Appointments Available':
+    pass
+  else:
+    tarihler.append((pd.to_datetime(i.split(' ')[4], format='%m/%d/%Y') - pd.to_datetime(date.today(), format='%Y/%m/%d')).days)
+argument = str(min(tarihler))
   
 
-gun_var = 'calculating'
-  
-if st.button('Calculate the Price'):
-  app_check(gun_var)
- 
-st.text(gun_var)
+
+st.text(argument)
