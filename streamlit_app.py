7cca5fa32bde4439bb7497524b0b48bb41ad0686
serhat <sguven1@yandex.com>
2022-04-02 12:20:48 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,9 +1,9 @@
 #!pip install selenium
 #!apt-get update # to update ubuntu to correctly run apt install
 #!apt install chromium-chromedriver
-!cp /usr/lib/chromium-browser/chromedriver /usr/bin
+#!cp /usr/lib/chromium-browser/chromedriver /usr/bin
 import sys
-sys.path.insert(0,'/usr/lib/chromium-browser/chromedriver')
+#sys.path.insert(0,'/usr/lib/chromium-browser/chromedriver')
 from selenium import webdriver
 import pandas as pd
 from datetime import date
