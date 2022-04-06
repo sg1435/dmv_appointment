9b25de7dbdc83a820393eff3beb1af4d1576640a
serhat <sguven1@yandex.com>
2022-04-06 06:06:02 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,61 +1,39 @@
-import sys
-from selenium import webdriver
-import pandas as pd
-from datetime import date
-from selenium.webdriver.common.by import By
 import streamlit as st
 
+class appointment_check(self):
+    def __init__(self):
+        import sys as self.sys
+        from selenium import webdriver as self.webdriver
+        import pandas as self.pd
+        from datetime import self.date
+        from selenium.webdriver.common.by import self.By
 
-
-def exam_counter():
-    chrome_options = webdriver.ChromeOptions()
-    chrome_options.add_argument('--headless')
-    chrome_options.add_argument('--no-sandbox')
-    chrome_options.add_argument('--disable-dev-shm-usage')
-    wd = webdriver.Chrome('chromedriver',chrome_options=chrome_options)
-    wd.get('https://telegov.njportal.com/njmvc/AppointmentWizard/19')
-    tarihler = []
-    listeler = [i.text for i in wd.find_elements(By.XPATH,"//*[contains(@id, 'dateText')]")]
-    for i in listeler:
-      if i == 'No Appointments Available':
-        pass
-      else:
-        tarihler.append((pd.to_datetime(i.split(' ')[4], format='%m/%d/%Y') - pd.to_datetime(date.today(), format='%Y/%m/%d')).days)
-    return str(min(tarihler))
- 
-def six_points_counter():
-    chrome_options = webdriver.ChromeOptions()
-    chrome_options.add_argument('--headless')
-    chrome_options.add_argument('--no-sandbox')
-    chrome_options.add_argument('--disable-dev-shm-usage')
-    wd = webdriver.Chrome('chromedriver',chrome_options=chrome_options)
-    wd.get('https://telegov.njportal.com/njmvc/AppointmentWizard/15')
-    tarihler = []
-    listeler = [i.text for i in wd.find_elements(By.XPATH,"//*[contains(@id, 'dateText')]")]
-    #butonlar = wd.find_elements(By.XPATH,"//*[contains(@id, 'makebtn')]")
-    #butonlar = [i.get_attribute('href') for i in butonlar]
-    for i in listeler:
-      if i == 'No Appointments Available':
-        pass
-      else:
-        tarihler.append((pd.to_datetime(i.split(' ')[4], format='%m/%d/%Y') - pd.to_datetime(date.today(), format='%Y/%m/%d')).days)
-    return str(min(tarihler))
-    #index = tarihler.index(min(tarihler))
-    #days = str(min(tarihler))
-    #link = butonlar.index(min(tarihler))
-
-
+        self.chrome_options = self.webdriver.ChromeOptions()
+        self.chrome_options.add_argument('--headless')
+        self.chrome_options.add_argument('--no-sandbox')
+        self.chrome_options.add_argument('--disable-dev-shm-usage')
+        self.wd = self.webdriver.Chrome('chromedriver',chrome_options=self.chrome_options)
+    def counter(self, website):
+        self.wd.get(website)
+        tarihler = []
+        listeler = [i.text for i in self.wd.find_elements(self.By.XPATH,"//*[contains(@id, 'dateText')]")]
+        for i in listeler:
+          if i == 'No Appointments Available':
+            pass
+          else:
+            tarihler.append((self.pd.to_datetime(i.split(' ')[4], format='%m/%d/%Y') - self.pd.to_datetime(self.date.today(), format='%Y/%m/%d')).days)
+        return str(min(tarihler))
 
 
 if st.button('DMV Initial Permit Nearest Appointment'):
-    st.text(six_points_counter() + ' DAYS TO NEAREST APPOINTMENT')
+    st.text(appointment_check.counter('https://telegov.njportal.com/njmvc/AppointmentWizard/15') + ' DAYS TO NEAREST APPOINTMENT')
     link = '[GO TO PAGE](https://telegov.njportal.com/njmvc/AppointmentWizard/15)'
     st.markdown(link, unsafe_allow_html=True)
 
 
     
 if st.button('DMV Exam Nearest Appointment'):
-    st.text(exam_counter() + ' DAYS TO NEAREST APPOINTMENT')
+    st.text(appointment_check.counter('https://telegov.njportal.com/njmvc/AppointmentWizard/19') + ' DAYS TO NEAREST APPOINTMENT')
     link = '[GO TO PAGE](https://telegov.njportal.com/njmvc/AppointmentWizard/19)'
     st.markdown(link, unsafe_allow_html=True)
 
